e5f53308675b3a33200ee09019d75a505ae0c7f6
Nathan Alden, Sr <me@nathanalden.com>
2013-04-07 10:43:23 -0500

    No longer preparing PostgreSQL commands; Prepare() does not perform well and prevents multiple statements

--- Source/Data.PostgreSql/CommandProvider.cs
+++ Source/Data.PostgreSql/CommandProvider.cs
@@ -34,7 +34,6 @@ namespace Junior.Data.PostgreSql
 			{
 				command.Parameters.Add(parameter);
 			}
-			command.Prepare();
 
 			return command;
 		}
--- Source/Data.PostgreSql/Properties/AssemblyInfo.cs
+++ Source/Data.PostgreSql/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 [assembly:ComVisible(false)]
 [assembly:AssemblyTitle("Junior.Data.PostgreSql")]
 [assembly:AssemblyVersion("1.0.0.0")]
-[assembly:AssemblyFileVersion("1.0.2.0")]
+[assembly:AssemblyFileVersion("1.0.3.0")]
