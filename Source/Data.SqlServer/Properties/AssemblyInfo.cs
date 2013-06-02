9e10804bd71ff43fb08941078bf2455dab39f44b
Nathan Alden, Sr <me@nathanalden.com>
2013-06-01 23:23:34 -0500

    Updated version numbers due to source code rollback Updated JuniorCommon to 3.3.1 Updated MySql.Data to 6.6.5

--- Source/Data.SqlServer/Properties/AssemblyInfo.cs
+++ Source/Data.SqlServer/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly:ComVisible(false)]
 [assembly:AssemblyTitle("Junior.Data.SqlServer")]
-[assembly:AssemblyVersion("1.0.0.0")]
-[assembly:AssemblyFileVersion("1.0.4.0")]
+[assembly:AssemblyVersion("3.0.0.0")]
+[assembly:AssemblyFileVersion("3.0.0.0")]
