44d1af95ad6f1eb5224fc6be6bd63263b398c337
Nathan Alden, Sr <me@nathanalden.com>
2013-06-13 15:30:17 -0500

    Added defaultValue parameter to data reader extension methods

--- Source/Data.MySql/Properties/AssemblyInfo.cs
+++ Source/Data.MySql/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly:ComVisible(false)]
 [assembly:AssemblyTitle("Junior.Data.MySql")]
-[assembly:AssemblyVersion("4.0.0.0")]
-[assembly:AssemblyFileVersion("4.0.0.0")]
+[assembly:AssemblyVersion("4.1.0.0")]
+[assembly:AssemblyFileVersion("4.1.0.0")]
--- Source/Data.SqlServer/SqlDataReaderExtensions.cs
+++ Source/Data.SqlServer/SqlDataReaderExtensions.cs
@@ -7,17 +7,25 @@ namespace Junior.Data.SqlServer
 {
 	public static class SqlDataReaderExtensions
 	{
-		public static T GetValue<T>(this SqlDataReader reader, string column)
+		public static T GetValue<T>(this SqlDataReader reader, string column, T defaultValue = default(T))
 		{
 			reader.ThrowIfNull("reader");
 			column.ThrowIfNull("column");
 
 			int ordinal = reader.GetOrdinal(column);
+
+			return GetValue(reader, ordinal, defaultValue);
+		}
+
+		public static T GetValue<T>(this SqlDataReader reader, int ordinal, T defaultValue = default(T))
+		{
+			reader.ThrowIfNull("reader");
+
 			Type type = typeof(T);
 
 			if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>) && reader.IsDBNull(ordinal))
 			{
-				return default(T);
+				return defaultValue;
 			}
 
 			return (T)reader.GetValue(ordinal);
