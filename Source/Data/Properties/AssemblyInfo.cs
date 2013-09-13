06a73bfb6af09743c9db7bee1858d666b3e418e6
Nathan Alden, Sr <me@nathanalden.com>
2013-09-12 20:54:45 -0500

    Corrected assembly version number

--- Source/Data/Properties/AssemblyInfo.cs
+++ Source/Data/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly:ComVisible(false)]
 [assembly:AssemblyTitle("Junior.Data")]
-[assembly:AssemblyVersion("5.0.2.0")]
-[assembly:AssemblyFileVersion("5.0.2.0")]
+[assembly:AssemblyVersion("5.0.0.0")]
+[assembly:AssemblyFileVersion("5.0.3.0")]
